8f3da01d398b8d8f667d50db11750c08129edffd
Philip Richardson <uberthought@gmail.com>
2019-09-14 15:01:23 -0400

    finished prototype of dash app

--- kiosk.sh
+++ kiosk.sh
@@ -1,10 +1,12 @@
+cd /home/pi/temperature
 
 export DISPLAY=:0
 xset s noblank
 xset s off
 xset -dpms
 unclutter -idle 0.5 -root &
-/usr/bin/python3 /home/pi/temperature/temp_flask/temp_flask.py &
+/usr/bin/python3 /home/pi/temperature/collect.py &
+/usr/bin/python3 /home/pi/temperature/server.py &
 sleep 5
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk --app=http://0.0.0.0:5000/
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk --app=http://192.168.1.189:8050/
 
--- server.py
+++ server.py
@@ -13,39 +13,29 @@ import csv
 app = dash.Dash()
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    html.Button('Refresh', id='refresh'),
-    html.Div(id='time-div', style={'font-size': '125px', 'text-align': 'center'}),
-    html.Div(id='date-div', style={'font-size': '50px', 'text-align': 'center'}),
-    html.Div(id='temperature-div', style={'font-size': '125px', 'text-align': 'center'}),
+    html.Div(id='time-div', style={'font-size': '100px', 'text-align': 'center'}),
+    html.Div(id='date-div', style={'font-size': '25px', 'text-align': 'center'}),
+    html.Div(id='temperature-div', style={'font-size': '100px', 'text-align': 'center'}),
     dcc.Graph(id='example-graph'),
     dcc.Interval(id='interval-component', interval=5*1000, n_intervals=1)
 ])
 
 @app.callback(
     dash.dependencies.Output(component_id='time-div', component_property='children'),
-    [dash.dependencies.Input('interval-component', 'n_intervals'),
-     dash.dependencies.Input('refresh', 'n_clicks')])
-def update_time_div(n_intervals, n_clicks):
+    [dash.dependencies.Input('interval-component', 'n_intervals')])
+def update_time_div(n_intervals):
     return str(datetime.now().strftime('%-I:%M%p'))
 
 @app.callback(
     dash.dependencies.Output(component_id='date-div', component_property='children'),
-    [dash.dependencies.Input('interval-component', 'n_intervals'),
-     dash.dependencies.Input('refresh', 'n_clicks')])
-def update_date_div(n_intervals, n_clicks):
+    [dash.dependencies.Input('interval-component', 'n_intervals')])
+def update_date_div(n_intervals):
     return str(datetime.now().strftime('%b %d %Y'))
 
 @app.callback(
     dash.dependencies.Output(component_id='temperature-div', component_property='children'),
-    [dash.dependencies.Input('interval-component', 'n_intervals'),
-     dash.dependencies.Input('refresh', 'n_clicks')])
-def update_temperature_div(n_intervals, n_clicks):
+    [dash.dependencies.Input('interval-component', 'n_intervals')])
+def update_temperature_div(n_intervals):
     with open('temperature.csv', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
         row = list(reader)[-1]
@@ -60,9 +50,8 @@ def update_temperature_div(n_intervals, n_clicks):
 
 @app.callback(
     dash.dependencies.Output(component_id='example-graph', component_property='figure'),
-    [dash.dependencies.Input('interval-component', 'n_intervals'),
-     dash.dependencies.Input('refresh', 'n_clicks')])
-def update_graph(n_intervals, n_clicks):
+    [dash.dependencies.Input('interval-component', 'n_intervals')])
+def update_graph(n_intervals):
 
     timestamps = []
     temperatures = []
@@ -85,11 +74,10 @@ def update_graph(n_intervals, n_clicks):
 
     return {
         'data': [
-            go.Scatter(x = timestamps, y = humidities, name = 'humidity', yaxis='y1', line = dict(color='blue')),
-            go.Scatter(x = timestamps, y = temperatures, name = 'temperature', yaxis='y2', line = dict(color='red')),
+            go.Scatter(x = timestamps, y = temperatures, name = 'temperature', yaxis='y1', line = dict(color='red')),
+            go.Scatter(x = timestamps, y = humidities, name = 'humidity', yaxis='y2', line = dict(color='blue')),
         ],
         'layout': go.Layout(
-            title='Double Y Axis Example',
             yaxis=dict(title='temperature'),
             yaxis2=dict(
                 title='humidity',
@@ -100,4 +88,4 @@ def update_graph(n_intervals, n_clicks):
     }
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='192.168.1.189')
+    app.run_server(debug=False, host='192.168.1.189')
