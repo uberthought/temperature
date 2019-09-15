ede41bb17e83ee11bb3e11f67eed45b65d7d1187
Philip Richardson <uberthought@gmail.com>
2019-09-15 14:52:23 -0400

    updated ui and fixed directory issue

--- collect.py
+++ collect.py
@@ -7,16 +7,13 @@ import Adafruit_DHT
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
-
 while True:
     now = datetime.now()
 
     humidity0, temperature0 = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
     timestamp0 = now
 
-#    print(temperature0, humidity0, timestamp0)
-
-    with open('temperature.csv', 'a+', newline='') as csvfile:
+    with open('/home/pi/temperature/temperature.csv', 'a+', newline='') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         spamwriter.writerow([timestamp0, temperature0, humidity0])
 
--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import csv
 
@@ -15,10 +15,9 @@ import numpy as np
 app = dash.Dash()
 
 app.layout = html.Div(children=[
-    html.Div(id='time-div', style={'font-size': '100px', 'text-align': 'center'}),
-    html.Div(id='date-div', style={'font-size': '25px', 'text-align': 'center'}),
-    html.Div(id='temperature-div', style={'font-size': '100px', 'text-align': 'center'}),
-    dcc.Graph(id='example-graph'),
+    html.Div(id='time-div', style={'font-size': '75px', 'text-align': 'center'}),
+    html.Div(id='temperature-div', style={'font-size': '75px', 'text-align': 'center'}),
+    dcc.Graph(id='example-graph', style={'height': '300px'}),
     dcc.Interval(id='interval-component', interval=5*1000, n_intervals=1)
 ])
 
@@ -29,53 +28,47 @@ def update_time_div(n_intervals):
     return str(datetime.now().strftime('%-I:%M%p'))
 
 @app.callback(
-    dash.dependencies.Output(component_id='date-div', component_property='children'),
-    [dash.dependencies.Input('interval-component', 'n_intervals')])
-def update_date_div(n_intervals):
-    return str(datetime.now().strftime('%b %d %Y'))
-
-@app.callback(
     dash.dependencies.Output(component_id='temperature-div', component_property='children'),
     [dash.dependencies.Input('interval-component', 'n_intervals')])
 def update_temperature_div(n_intervals):
-    with open('temperature.csv', newline='') as csvfile:
+    with open('/home/pi/temperature/temperature.csv', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
         row = list(reader)[-1]
         temperature0 = float(row[1])
-        humidity0 = float(row[2])
         temperature0 = temperature0 * 1.8 + 32
 
-    temperature_str = '{0:.1f}'.format(temperature0)
-    humidity_str = '{0:.1f}'.format(humidity0)
-
-    return '{}F/{}%'.format(temperature_str, humidity_str)
+    return '{0:.1f}'.format(temperature0) + 'F'
 
 @app.callback(
     dash.dependencies.Output(component_id='example-graph', component_property='figure'),
     [dash.dependencies.Input('interval-component', 'n_intervals')])
 def update_graph(n_intervals):
 
-    with open('temperature.csv', newline='') as csvfile:
+    with open('/home/pi/temperature/temperature.csv', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
 
-        foo = np.array(list(reader))
-        timestamps = foo[:,0]
-        temperatures = foo[:,1].astype(float) * 1.8 + 32
-        humidities = foo[:,2].astype(float)
+        oldest = str(datetime.now() - timedelta(days=7))
+
+        timestamps = []
+        temperatures = []
+
+        for row in reader:
+            try:
+                if row[0] < oldest:
+                    continue
+                timestamps.append(row[0])
+                temperatures.append(float(row[1]) * 1.8 + 32)
+            except ValueError:
+                continue
+        
+#        foo = [r for r in foo if datetime.strptime(r[0], '%Y-%m-%d %H:%M:%S.%f') > oldest]
+#        temperatures = foo[:,1].astype(float) * 1.8 + 32
 
     return {
         'data': [
-            go.Scatter(x = timestamps, y = temperatures, name = 'temperature', yaxis='y1', line = dict(color='red')),
-            go.Scatter(x = timestamps, y = humidities, name = 'humidity', yaxis='y2', line = dict(color='blue')),
+            go.Scatter(x = timestamps, y = temperatures, name = 'temperature', yaxis='y1', line = dict(color='black')),
         ],
-        'layout': go.Layout(
-            yaxis=dict(title='temperature'),
-            yaxis2=dict(
-                title='humidity',
-                overlaying='y',
-                side='right'
-            )
-        )
+        'layout': go.Layout(yaxis=dict(title='temperature'))
     }
 
 if __name__ == '__main__':
